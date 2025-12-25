293321750a672506a4c193ab69e233e6ddac73af
al-paron <paronnikov4@gmail.com>
2025-12-25 18:19:52 +0300

    fixed import

--- src/main/java/org/web/RegisterServlet.java
+++ src/main/java/org/web/RegisterServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.Optional;
 
 @WebServlet("/register")
 public class RegisterServlet extends BaseServlet {
